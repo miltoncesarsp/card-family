2a0eea8d45ba9471c947d97b44603513352ae8c5
Milton César <89351176+miltoncesarsp@users.noreply.github.com>
2025-11-18 12:49:22 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,9 @@
+console.log("Supabase Client Loaded");
+
 // 🔥 Mude para suas chaves verdadeiras
 const SUPABASE_URL = "https://ujpbdoykjrntqketqjxa.supabase.co"; 
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqcGJkb3lranJudHFrZXRxanhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NTcxNDAsImV4cCI6MjA3OTAzMzE0MH0.kYstg_WVcsAANWWf942_qhrJLYyrPRR_gbN83kIqQxQ"; // sua anon key
 
-// Inicializa o cliente Supabase
+console.log("Criando cliente...");
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+console.log("Cliente criado:", supabase);
